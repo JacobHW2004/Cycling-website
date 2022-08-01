3f0a7c11fd17f32d5739eb47a2c3e9160fc20063
JacobHW2004 <110376542+JacobHW2004@users.noreply.github.com>
2022-08-01 08:27:05 +0100

    Add files via upload

---
+++ responses.js
@@ -0,0 +1,18 @@
+function getBotResponse(input) {
+    //responses
+    if (input == "Bike-clubs") {
+        return "No matter where you are located, there will be a bike club for you. Some of the most popular bike clubs in the uk are 'Cycling Touring Club' 'london Clarion Cycling Club' and 'Oxford University Cycling Club'";
+    } else if (input == "Bike-issues") {
+        return "If you are experiencing some bike issues whether it be comfort and ride quality, or lack of performance, the best place to start is by checking the tires. A flat tire can majorly decrease both speed and comfort, after checking your tires if you are still experiencing the same problems then you can try checking your handle bars and seat adjustability. Having lose handle bars will affect your bikes handling and stability during performance and having a lose bike seat can affect the support and suspension. If all these fail then its best to contact the bikes supplier for further assistance.  ";
+    } else if (input == "Types-of-bike-race") {
+        return "The main types of bike racing are: road, track, mountain and BMX. All of which you can find more detailed information about through our webiste. ";
+    } else if(input == "Best-locations-UK"){
+        return "Some of the most populaor locations in the uk are 'nation cycle route 72' 'Camel Trail' NC 500' and 'Monsal Trail'. More information and bike routes can be found on websites like redbull and Komoot.com";
+    } else if(input == "Where-to-buy-a-bike"){
+        return "When it comes to purchasing a bike you have a number of options to chose from, you can simply buy one from a shop such as halfords or evanscycles, or you could buy second hand for cheaper. Many companies offer the cycle to work shceme which will aline with your work place and allow you to get a huge discount from your bike.";
+    } else if(input == "How-to-become-a-member"){
+        return "You can become a member of our website by registering using the form located on our home page, doing this allows you to gain access to the monthly newsletter if you wish where you will receive an email filled with information ";
+    }
+
+    
+}
